2b13c95952be685ef4b2d792d09c1d587e36884e
Shlok Radadia <shlok2006radadia@gmail.com>
2025-12-17 23:06:27 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import urlRoutes from "./routes/url.js"
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"]
+}));
+app.use(express.json());
+
+app.use("/", urlRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("Error connecting to MongoDB: ", err);
+});
